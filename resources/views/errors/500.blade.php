080e4ca298d8192bc8b571c21812c0f90fc2f7fb
pzy.me <justmepzy@gmail.com>
2015-06-04 13:16:01 +0800

    update error page title

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,20 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Glazzed Admin Theme :: Error 404</title>
-
+    <title>Internal Server Error</title>
     <link rel="icon" sizes="192x192" href="/glazzed/img/touch-icon.png" />
     <link rel="apple-touch-icon" href="/glazzed/img/touch-icon-iphone.png" />
     <link rel="apple-touch-icon" sizes="76x76" href="/glazzed/img/touch-icon-ipad.png" />
     <link rel="apple-touch-icon" sizes="120x120" href="/glazzed/img/touch-icon-iphone-retina.png" />
     <link rel="apple-touch-icon" sizes="152x152" href="/glazzed/img/touch-icon-ipad-retina.png" />
-
     <link rel="shortcut icon" type="image/x-icon" href="/glazzed/img/favicon.ico" />
-
     <link rel="stylesheet" type="text/css" href="/glazzed/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/glazzed/css/main.min.css">
-
-    <!-- Pixeden Icon Fonts -->
     <link rel="stylesheet" type="text/css" href="/glazzed/css/pe-icon-7-filled.css">
     <link rel="stylesheet" type="text/css" href="/glazzed/css/pe-icon-7-stroke.css">
 </head>
@@ -24,7 +19,6 @@
 <div id="loading">
     <div class="loader loader-light loader-large"></div>
 </div>
-
 <div>
     <div class="col-md-12">
         <div class="main-logo error-page"><img src="/glazzed/img/logo.png"></div>
@@ -43,7 +37,6 @@
         </div>
     </div>
 </div>
-
 <script type="text/javascript" src="/glazzed/js/main.js"></script>
 </body>
 </html>
